5caa23a35325baf97bb376e3c81cc61bd8c9fecf
Terada <shin,yakishi222@gmail.com>
2018-01-30 15:24:54 +0900

    UniRxの追加

---
+++ PsychopathGameOfTag/Assets/Naihu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Naihu : MonoBehaviour {
+
+    private void OnTriggerStay(Collider col)
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && col.tag == "Enemy")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
